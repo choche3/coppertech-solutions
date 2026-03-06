1f38d9e9d7892c4495a3a9a4b361fc09c5db9369
Joseph Mwale <mwalejoseph138@gmail.com>
2026-03-06 17:26:29 +0200

    corrected a spelling on the form

--- js/main.js
+++ js/main.js
@@ -9,18 +9,19 @@ if (menuToggle){
     });
 }
 
+//mini local database
 const testimonials = [
     {
         text:"CopperTech Solutions fixed my laptop in record time",
-        author: "Happy Customer"
+        author: "~ Happy Customer"
     },
     {
         text: "Professional service and affordable pricing",
-        author: "Small Business Owner"
+        author: "~ Small Business Owner"
     },
     {
         text: "Highly recommend CopperTech for IT support",
-        author: "Local Entrepreneur"
+        author: "~ Local Entrepreneur"
     }
 ];
 
@@ -30,14 +31,16 @@ const textE1 = document.getElementById("testimonial-text");
 const authorE1 = document.getElementById("testimonial-author");
 
 function updateTestimonial(){
-    if(!textE1 || !authorE1) return;
+    if(!textEl || !authorEl) return;
 
-    textE1.textContent = testimonials[currentTestimonial].text;
-    authorE1.textContent = testimonials[currentTestimonial].author;
+    textEl.textContent = testimonials[currentTestimonial].text;
+    authorEl.textContent = testimonials[currentTestimonial].author;
 
+    //%testimonials.length means loop back to 0 when reaching the end
     currentTestimonial= (currentTestimonial + 1)% testimonials.length;
 }
 
+//the time it will take to run (updateTestimonial) or switch the testinmonials
 setInterval(updateTestimonial, 4000);
 
 
@@ -50,10 +53,10 @@ if (form){
         e.preventDefault();
 
         statusMsg.textContent = "sending message...";
-        statusMsg.style.color = "0A2540";
+        statusMsg.style.color = "#0A2540";
 
         setTimeout(() =>{
-            statusMsg.textContent = "Message sent successfully! we'll get bak to you shortly.";
+            statusMsg.textContent = "Message sent successfully! we'll get back to you shortly.";
             statusMsg.style.color = "green";
             form.reset();
         }, 1500);
